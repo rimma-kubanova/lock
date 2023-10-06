c677438cfaef683151b1f644bb15be3d46eb2cc9
rimskii <rimma.kubanova@gmail.com>
2023-10-07 01:07:31 +0600

    added text_req.txt && test cases

--- test_task.py
+++ test_task.py
@@ -7,7 +7,7 @@ import time
 
 next_time_to_execute = datetime.now()
 cron_execute_every_hour = '* */1 * * *'
-
+# owner_name = 'server 6'
 def run_task():
     # Код который должен быть выполнен только 1 раз за час
     # только на 1 машине
@@ -23,36 +23,38 @@ def main_loop():
             # Значит выполнять код можно и никто кроме этой машины
             # его в данный момент не выполнит.
             # Здесь нет except значит рейзить ошибки нельзя
-            if db_lock.aquire(owner_name,'backup_lock_2'):
+            if db_lock.aquire(owner_name,'backup_lock_3'):
                 run_task()
             else:
                 print("queue: task is already running")
             cron = croniter(cron_execute_every_hour, current_time)
             next_time_to_execute = cron.get_next(datetime)
         sleep(1)
-        
-def simulate_process(process_id):
+
+
+# TESTING: runnning 5 tasks at the same time. Expecting 1 task to perform backup, others just skipping it.
+def simulate(process_id):
     owner_name = f"server_{process_id}"
     
     if db_lock.aquire(owner_name,"new_lock"):
-        print(f"{owner_name} acquired the lock and is performing the backup...")
-        # Simulate backup process
+        print(f"{owner_name} acquired the lock & performing backup...")
         time.sleep(5)
-        print(f"{owner_name}'s backup completed.")
+        print(f"{owner_name} backup completed!")
     else:
-        print(f"{owner_name} couldn't acquire the lock. Another process is already performing the backup.")
+        print(f"{owner_name} couldn't acquire the lock. Another process is already performing it")
 
 
 if __name__ == "__main__":
     db_lock = db_lock.Lock()
     # main_loop()
     
+    
+    # testing, simulating 5 tasks running at the same time using threading
     processes = []
-    for i in range(1, 6):  # Simulate 5 processes
-        process = threading.Thread(target=simulate_process, args=(i,))
+    for i in range(1, 6):
+        process = threading.Thread(target=simulate, args=(i,))
         processes.append(process)
         process.start()
 
-    # Wait for all threads to finish
     for process in processes:
         process.join()
